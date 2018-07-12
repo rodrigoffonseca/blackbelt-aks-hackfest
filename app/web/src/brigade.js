a9f5d907290c92544ab2811b8208f57d73a477e7
Rodrigo Fonseca <rodrigoffonseca@gmail.com>
2018-07-12 14:27:53 -0300

    Delete brigade.js

--- app/web/src/brigade.js
+++
@@ -1,7 +0,0 @@
-function kubeJobRunner (config, k) {
-    k.storage.enabled = false
-    k.image = "lachlanevenson/k8s-kubectl:v1.8.2"
-    k.tasks = [
-        `kubectl set image deployment/heroes-web-deploy heroes-web-cntnr=focaacr.azurecr.io/azureworkshop/rating-web:${config.get("imageTag")}`
-    ]
-}
