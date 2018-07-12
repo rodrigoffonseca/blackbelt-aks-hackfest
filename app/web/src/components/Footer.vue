b312aa1d0ae5c1b6b39275b8272af40706b557d8
Rodrigo Fonseca <rodrigoffonseca@gmail.com>
2018-07-12 12:51:31 -0300

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Marvel Rules!!! Avengers >>>> ALL
+        Marvel Rules!!! Avengers >>>> ALL (Powered by BRIGADE)
       </div>
       <div class="col-lg-6">
       </div>
